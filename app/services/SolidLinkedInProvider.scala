c00ba3bd4abbceeb3ca52dd142c1d708d5a4176d
Mike Pigott <mpigott@ironhorsesoftware.tech>
2019-07-24 16:27:31 -0400

    Bearer token goes in the HTTP headers.

--- app/services/SolidLinkedInProvider.scala
+++ app/services/SolidLinkedInProvider.scala
@@ -1,9 +1,13 @@
 package services
 
+import scala.concurrent.Future
+
 import com.mohiva.play.silhouette.api.util.HTTPLayer
-import com.mohiva.play.silhouette.impl.providers.{SocialStateHandler, OAuth2Settings, CommonSocialProfile, CommonSocialProfileBuilder }
-import com.mohiva.play.silhouette.impl.providers.oauth2.BaseLinkedInProvider
-import com.mohiva.play.silhouette.impl.providers.oauth2.LinkedInProvider
+import com.mohiva.play.silhouette.impl.exceptions.ProfileRetrievalException
+import com.mohiva.play.silhouette.impl.providers.{SocialStateHandler, CommonSocialProfile, CommonSocialProfileBuilder }
+import com.mohiva.play.silhouette.impl.providers.{OAuth2Settings, OAuth2Info}
+import com.mohiva.play.silhouette.impl.providers.oauth2.{BaseLinkedInProvider, LinkedInProvider}
+import com.mohiva.play.silhouette.impl.providers.oauth2.LinkedInProvider.SpecifiedProfileError
 
 class SolidLinkedInProvider(
     protected val httpLayer : HTTPLayer,
@@ -14,13 +18,28 @@ class SolidLinkedInProvider(
 
   override val profileParser = new SolidLinkedInProfileParser
 
+  override protected def buildProfile(authInfo: OAuth2Info): Future[Profile] = {
+    httpLayer.url(urls("api")).withHttpHeaders(("Authorization", s"Bearer ${authInfo.accessToken}")).get().flatMap { response =>
+      val json = response.json
+      (json \ "errorCode").asOpt[Int] match {
+        case Some(error) =>
+          val message = (json \ "message").asOpt[String]
+          val requestId = (json \ "requestId").asOpt[String]
+          val status = (json \ "status").asOpt[Int]
+          val timestamp = (json \ "timestamp").asOpt[Long]
+
+          Future.failed(new ProfileRetrievalException(SpecifiedProfileError.format(id, error, message, requestId, status, timestamp)))
+        case _ => profileParser.parse(json, authInfo)
+      }
+    }
+  }
+
   override def withSettings(f : (Settings) => Settings) = {
     new SolidLinkedInProvider(httpLayer, stateHandler, f(settings))
   }
 }
 
 object SolidLinkedInProvider {
-  val SpecifiedProfileError = LinkedInProvider.SpecifiedProfileError
   val ID = LinkedInProvider.ID
 
   val API = "https://api.linkedin.com/v2/me"
