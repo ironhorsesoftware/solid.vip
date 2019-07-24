fd7b40bd0bebf93d7d5c77cbf0fff9fd95db2526
Mike Pigott <mpigott@ironhorsesoftware.tech>
2019-07-23 20:20:52 -0400

    Attempting to fix the ExtendedGitHubProvider.

--- app/services/ExtendedGitHubProvider.scala
+++ app/services/ExtendedGitHubProvider.scala
@@ -1,22 +1,34 @@
 package services
 
 import com.mohiva.play.silhouette.api.util.HTTPLayer
-import com.mohiva.play.silhouette.impl.providers.{SocialStateHandler, OAuth2Settings, CommonSocialProfile}
-import com.mohiva.play.silhouette.impl.providers.oauth2.BaseGitHubProvider
+import com.mohiva.play.silhouette.impl.providers.{SocialStateHandler, OAuth2Settings, CommonSocialProfile, CommonSocialProfileBuilder }
+import com.mohiva.play.silhouette.impl.providers.oauth2.{BaseGitHubProvider, GitHubProvider}
 
 
 class ExtendedGitHubProvider (
     protected val httpLayer : HTTPLayer,
     protected val stateHandler : SocialStateHandler,
-    val settings : OAuth2Settings) extends BaseGitHubProvider {
+    val settings : OAuth2Settings) extends BaseGitHubProvider with CommonSocialProfileBuilder  {
 
-  type Self = ExtendedGitHubProvider
+  override type Self = ExtendedGitHubProvider
 
-  type Profile = CommonSocialProfile
+  override val profileParser = new ExtendedGitHubProfileParser
 
-  val profileParser = new ExtendedGitHubProfileParser
-
-  def withSettings(f : (Settings) => Settings) = {
+  override def withSettings(f : (Settings) => Settings) = {
     new ExtendedGitHubProvider(httpLayer, stateHandler, f(settings))
   }
+}
+
+object ExtendedGitHubProvider {
+  /**
+   * The error messages.
+   */
+  val SpecifiedProfileError = GitHubProvider.SpecifiedProfileError
+
+  /**
+   * The GitHub constants.
+   */
+  val ID = GitHubProvider.ID
+  val API = GitHubProvider.API
+
 }
