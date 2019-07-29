6d347edcd6fea9172a83990127483c06b833ae01
Mike Pigott <mpigott@ironhorsesoftware.tech>
2019-07-29 10:56:54 -0400

    Profile Parser for Twitter

--- app/services/SolidTwitterProfileParser.scala
+++ app/services/SolidTwitterProfileParser.scala
@@ -14,8 +14,23 @@ import com.mohiva.play.silhouette.impl.providers.SocialProfileParser
 import models.Profile
 
 class SolidTwitterProfileParser extends SocialProfileParser[JsValue, Profile, OAuth1Info] with Logging {
-  override def parse(json : JsValue, authOinfo: OAuth1Info) : Future[Profile] = {
+  override def parse(json : JsValue, authOinfo: OAuth1Info) : Future[Profile] = Future.successful {
     logger.info(s"JSON: ${json}")
-    Future.failed(new UnsupportedOperationException)
+
+    Profile(
+        loginInfo = LoginInfo(SolidTwitterProvider.ID, (json \ "id").as[JsNumber].toString),
+        name = (json \ "name").as[String],
+        picture = (json \ "profile_image_url_https").asOpt[String],
+        title = None,
+        summary = (json \ "description").asOpt[String],
+        location = (json \ "location").asOpt[String],
+        email = (json \ "email").asOpt[String],
+        website = (json \ "url").asOpt[String],
+        twitterUrl = (json \ "screen_name").asOpt[String].map (screenName => s"https://twitter.com/${screenName}"),
+        gitHubUrl = None,
+        gitHubUsername = None,
+        projects = List(),
+        workExperience = List()
+    )
   }
 }
